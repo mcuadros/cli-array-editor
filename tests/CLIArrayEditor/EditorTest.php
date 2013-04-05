f944abee92e65c626f4149eb37bb1f55ca4b597d
Máximo Cuadros Ortiz <mcuadros@gmail.com>
2013-04-05 21:11:30 +0200

    LICENSE file

--- tests/CLIArrayEditor/EditorTest.php
+++ tests/CLIArrayEditor/EditorTest.php
@@ -1,7 +1,17 @@
 <?php
+/*
+ * This file is part of the CLIArrayEditor package.
+ *
+ * (c) Máximo Cuadros <mcuadros@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+ 
 require __DIR__ . '/../../vendor/autoload.php';
 use CLIArrayEditor\Editor;
 use CLIArrayEditor\Format\JSON;
+use CLIArrayEditor\Format\YAML;
 
 
 class EditorTest extends PHPUnit_Framework_TestCase
@@ -16,7 +26,8 @@ class EditorTest extends PHPUnit_Framework_TestCase
         $editor = new Editor();
         $result = $editor
             ->setEditor('vim')
-            ->setFormat(new JSON)
+            ->setForceModify(true)
+            ->setFormat(new YAML)
             ->edit($tmp);
             
         print_r($result);

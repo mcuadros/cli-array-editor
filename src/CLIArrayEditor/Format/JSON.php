acefc75948203b249bb59e2575fdd958cb11e103
Máximo Cuadros Ortiz <mcuadros@gmail.com>
2013-04-06 00:44:17 +0200

    pretty print 5.3

--- src/CLIArrayEditor/Format/JSON.php
+++ src/CLIArrayEditor/Format/JSON.php
@@ -21,7 +21,11 @@ class JSON implements Format
      */
     public function to(array $data)
     {
-        return json_encode($data, JSON_PRETTY_PRINT);
+        if (version_compare(PHP_VERSION, '5.4.0') >= 0) {
+            return json_encode($data, JSON_PRETTY_PRINT);
+        }
+
+        return $this->pretty(json_encode($data));
     }
 
     /**
@@ -30,5 +34,69 @@ class JSON implements Format
     public function from($data)
     {
         return json_decode($data, true);
+    }
+
+    /**
+     * Pretty print for JSON in PHP 5.3
+     *
+     * @link http://snipplr.com/view/60559/prettyjson/
+     * @param string $json
+     * 
+     * @return string
+     */
+    private function pretty($json)
+    {
+     
+        $result      = '';
+        $pos         = 0;
+        $strLen      = strlen($json);
+        $indentStr   = '    ';
+        $newLine     = "\n";
+        $prevChar    = '';
+        $outOfQuotes = true;
+     
+        for ($i=0; $i<=$strLen; $i++) {
+     
+            // Grab the next character in the string.
+            $char = substr($json, $i, 1);
+     
+            // Are we inside a quoted string?
+            if ($char == '"' && $prevChar != '\\') {
+                $outOfQuotes = !$outOfQuotes;
+     
+            // If this character is the end of an element, 
+            // output a new line and indent the next line.
+            } else if(($char == '}' || $char == ']') && $outOfQuotes) {
+                $result .= $newLine;
+                $pos --;
+                for ($j=0; $j<$pos; $j++) {
+                    $result .= $indentStr;
+                }
+            }
+     
+            // Add the character to the result string.
+            $result .= $char;
+     
+            // If the last character was the beginning of an element, 
+            // output a new line and indent the next line.
+            if (($char == ',' || $char == '{' || $char == '[') && $outOfQuotes) {
+                $result .= $newLine;
+                if ($char == '{' || $char == '[') {
+                    $pos ++;
+                }
+     
+                for ($j = 0; $j < $pos; $j++) {
+                    $result .= $indentStr;
+                }
+            }
+     
+            if (($char == ':') && $outOfQuotes) {
+                $result .= ' ';
+            }
+
+            $prevChar = $char;
+        }
+     
+        return $result;
     }
 }
